48ccfb8007c148ee7de9ab436554ff6faae9e54d
bima020 <64942799+bimasena321@users.noreply.github.com>
2022-11-05 20:54:43 +0700

    Add files via upload

---
+++ url.py
@@ -0,0 +1,33 @@
+import webbrowser
+
+#def cari(cari):
+   # aa = webbrowser.open(f'https://www.google.com/search?q={cari}&hl=id&sxsrf=ALiCzsYAf2FPJRiNLWpg9k57pSFHg_IYFw%3A1667361165614&source=hp&ei=jelhY4iqIca7seMPmZmU2AQ&iflsig=AJiK0e8AAAAAY2H3naOBIVAX7fsq98itCGCuLHCDCohW&oq=ind&gs_lp=Egdnd3Mtd2l6uAEB-AEBKgIIADIEECMYJzIEECMYJzIEECMYJzIKEC4YsQMY1AIYQzIIEAAYsQMYgwEyBBAAGEMyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIEEC4YQzIIEAAYgAQYyQPCAgcQIxjqAhgnwgIHEC4Y1AIYQ8ICCxAuGIAEGLEDGIMBqAIKSNQXUO8FWIIIcAF4AMgBAJABAJgBcaABrAKqAQMyLjE&sclient=gws-wiz')
+   # print(aa)
+#def yt(cari1):
+ #   aa1 = webbrowser.open(f'https://www.youtube.com/results?search_query={cari1}')
+  #  print (aa1)
+  
+def cari(cari):
+    aa = webbrowser.open('https://www.google.com')
+    print(aa)
+def yt(a):
+    aa = webbrowser.open('https://www.youtube.com/')
+    print(aa)
+def ig():
+    bb = webbrowser.open('https://www.instagram.com/')
+    print (bb)
+def fb():
+    cc = webbrowser.open('https://www.facebook.com/')
+    print (cc)
+def word():
+    dd = webbrowser.open('https://www.office.com/launch/word?ui=id-ID&rs=ID&auth=1')
+    print (dd)
+def excel():
+    ee = webbrowser.open('https://www.office.com/launch/excel?ui=id-ID&rs=ID&auth=1')
+    print (ee)
+def ppt():
+    ff = webbrowser.open('https://www.office.com/launch/powerpoint?ui=id-ID&rs=ID&auth=1')
+    print (ff)
+def ytgame(g):
+    hh = webbrowser.open('https://www.youtube.com/watch?v=M4_k_7K-YxY')
+    print (hh)
